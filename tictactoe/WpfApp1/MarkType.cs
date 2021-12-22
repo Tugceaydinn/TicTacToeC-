d695a0be1d6618caca131b73c06270fd53f17148
Tugceaydinn <91567840+Tugceaydinn@users.noreply.github.com>
2021-12-22 20:24:02 +0300

    Add files via upload

---
+++ tictactoe/WpfApp1/MarkType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public enum MarkType
+    {
+        /// <summary>
+        /// The cell hasn't been clicked yet
+        /// </summary>
+        Free,
+        /// <summary>
+        /// The cell is a O
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// The cell is an X
+        /// </summary>
+        Cross
+    }
+
+}
